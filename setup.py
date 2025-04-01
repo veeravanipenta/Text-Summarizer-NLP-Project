ee54ce50e14857e86ebcf08a113df2932975f20c
veerareddy <vanipentaveerareddy1998@gmail.com>
2025-04-01 13:14:47 -0600

    requirements installation done

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer-NLP-Project"
+AUTHOR_USER_NAME = "veeravanipenta"
+SRC_REPO = "TextSummarizer"
+AUTHOR_EMAIL = "vanipentaveerareddy1998@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description = "A small python package for NLP app",
+    long_description = long_description,
+    long_description_content="text/markdown",
+    url="https://github.com/" + AUTHOR_USER_NAME + "/" + REPO_NAME,
+    project_urls = {
+        "Bug Tracker": "https://github.com/" + AUTHOR_USER_NAME + "/" + REPO_NAME + "/issues",
+    },
+    package_dir={"": "src"},
+    packages = setuptools.find_packages(where="src")
+)
+    
+    
+    
